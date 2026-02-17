db3d211678ce51d26adaa3b1d018a713e42d1dae
nathanchamorro1 <160960078+nathanchamorro1@users.noreply.github.com>
2026-02-16 18:20:38 -0800

    Added structure of website, fonts, CSS, HTML, JS, and style

---
+++ .history/js/script_20260216174957.js
@@ -0,0 +1,16 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach(el => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add("reveal-visible");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
